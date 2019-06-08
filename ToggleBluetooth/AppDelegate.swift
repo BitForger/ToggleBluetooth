649be5c2a8e3cffb557416110eab21aee8f8d465
Noah Kovacs <kovacsn66@gmail.com>
2019-06-08 12:10:01 -0500

    Initial Commit

---
+++ ToggleBluetooth/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ToggleBluetooth
+//
+//  Created by Noah Kovacs on 6/8/19.
+//  Copyright © 2019 Noah Kovacs. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ToggleBluetooth/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ToggleBluetooth
+//
+//  Created by Noah Kovacs on 6/8/19.
+//  Copyright © 2019 Noah Kovacs. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
